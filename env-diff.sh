65a5414e4d42bc2e5e36b2a0fbceeed8d73e9396
charlotte-z <2698028166@qq.com>
2023-08-14 22:27:50 +0800

    fix the script which is not correct to handle the key value

--- env-diff.sh
+++ env-diff.sh
@@ -1,56 +1,55 @@
 #!/bin/bash
 
-# Create a temporary file to store keys in the second file
 keys_in_file2=$(mktemp)
 
-# Process the second file (the new env)
-cat "${2}" | tr -s '\n' | while read line || [[ -n ${line} ]]; do
-    check_is_start_with_comment=$(echo ${line} | grep "^#")
-    if [[ -n ${check_is_start_with_comment} ]]; then
-        continue;
-    fi
+get_key() {
+    echo "$1" | cut -d= -f1
+}
 
-    key=(${line//=/ })
-    echo "${key[0]}=${key[1]}"
-    echo "${key[0]}" >> ${keys_in_file2}
-done
+get_value() {
+    echo "$1" | cut -d= -f2-
+}
 
-# Process the first file (the old env)
-cat "${1}" | tr -s '\n' | while read line || [[ -n ${line} ]]; do
-    check_is_start_with_comment=$(echo ${line} | grep "^#")
-    if [[ -n ${check_is_start_with_comment} ]]; then
-        continue;
-    fi
-
-    key=(${line//=/ })
-    value=${line/$key'='/}
-
-    # If the key exists in the second file, skip this line
-    exists=$(grep "^${key[0]}$" ${keys_in_file2})
-    if [[ -n ${exists} ]]; then
+cat "$2" | tr -s '\n' | while read line || [[ -n $line ]]; do
+    if [[ $line == \#* ]]; then
         continue
     fi
 
-    echo "${key[0]}=${value}"
+    key=$(get_key "$line")
+    value=$(get_value "$line")
+    echo "$key=$value"
+    echo "$key" >> $keys_in_file2
 done
 
-# Process the second file again to add keys not found in the first file
-cat "${2}" | tr -s '\n'  | while read line || [[ -n ${line} ]]
-do
-    a=$(echo ${line} | grep "^#")
-    if [[ -n ${a} ]]; then
-        continue;
+cat "$1" | tr -s '\n' | while read line || [[ -n $line ]]; do
+    if [[ $line == \#* ]]; then
+        continue
     fi
 
-    key=(${line//=/ })
-    data=$(grep "${key[0]}=" $1)
-    value=${line/$key'='/}
+    key=$(get_key "$line")
+    value=$(get_value "$line")
 
-    # If data is empty string and the value is not "value_undefined_flag"
-    if [[ -z ${data} ]] && [[ ${value} != "value_undefined_flag" ]]; then
-        echo "${key[0]}=${value}"
+    exists=$(grep "^$key$" $keys_in_file2)
+    if [[ -n $exists ]]; then
+        continue
+    fi
+
+    echo "$key=$value"
+done
+
+cat "$2" | tr -s '\n' | while read line || [[ -n $line ]]; do
+    if [[ $line == \#* ]]; then
+        continue
+    fi
+
+    key=$(get_key "$line")
+    value=$(get_value "$line")
+
+    data=$(grep "$key=" "$1")
+
+    if [[ -z $data ]] && [[ $value != "value_undefined_flag" ]]; then
+        echo "$key=$value"
     fi
 done
 
-# Remove the temporary file
-rm ${keys_in_file2}
+rm $keys_in_file2
