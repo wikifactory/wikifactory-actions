8e9f7a672d343f04932879a1fe60b6871a017dfd
charlotte-z <2698028166@qq.com>
2023-08-14 23:02:52 +0800

    fix the script to handle the value undefined flag

--- env-diff.sh
+++ env-diff.sh
@@ -7,7 +7,7 @@ get_key() {
 }
 
 get_value() {
-    echo "$1" | cut -d= -f2-
+    echo "$1" | cut -d= -f2
 }
 
 cat "$2" | tr -s '\n' | while read line || [[ -n $line ]]; do
@@ -17,9 +17,15 @@ cat "$2" | tr -s '\n' | while read line || [[ -n $line ]]; do
 
     key=$(get_key "$line")
     value=$(get_value "$line")
+
+    if [[ $value == "value_undefined_flag" ]]; then
+        continue
+    fi
+
     echo "$key=$value"
     echo "$key" >> $keys_in_file2
 done
+
 
 cat "$1" | tr -s '\n' | while read line || [[ -n $line ]]; do
     if [[ $line == \#* ]]; then
@@ -35,21 +41,6 @@ cat "$1" | tr -s '\n' | while read line || [[ -n $line ]]; do
     fi
 
     echo "$key=$value"
-done
-
-cat "$2" | tr -s '\n' | while read line || [[ -n $line ]]; do
-    if [[ $line == \#* ]]; then
-        continue
-    fi
-
-    key=$(get_key "$line")
-    value=$(get_value "$line")
-
-    data=$(grep "$key=" "$1")
-
-    if [[ -z $data ]] && [[ $value != "value_undefined_flag" ]]; then
-        echo "$key=$value"
-    fi
 done
 
 rm $keys_in_file2
