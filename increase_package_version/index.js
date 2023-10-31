ae04e4f7bf18e8b9118d864110db0f7012457a75
Antique <33049468+Charlotte-z@users.noreply.github.com>
2023-10-31 14:25:03 +0800

    implement the auto increasement for package json (#37)
    * implement the auto increasement
    
    * rename the action
    
    * output the environment

---
+++ increase_package_version/index.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const path = require("path");
+const core = require("@actions/core");
+const process = require('process');
+
+try {
+  function incrementVersion(version) {
+    const parts = version.split(".");
+    if (parts[2] < 9) {
+      parts[2]++;
+    } else {
+      parts[2] = 0;
+      if (parts[1] < 9) {
+        parts[1]++;
+      } else {
+        parts[1] = 0;
+        parts[0]++;
+      }
+    }
+    return parts.join(".");
+  }
+
+  const pathInput = core.getInput("path");
+  const fullPath = path.join(process.env.GITHUB_WORKSPACE, pathInput);
+
+  const packageDataRaw = fs.readFileSync(fullPath, 'utf8');
+  const packageData = JSON.parse(packageDataRaw);
+  
+  const oldVersion = packageData.version;
+  packageData.version = incrementVersion(packageData.version);
+  const newVersion = packageData.version;
+
+  fs.writeFileSync(
+    fullPath,
+    JSON.stringify(packageData, null, 2) + "\n"
+  );
+  
+  core.setOutput("old-version", oldVersion);
+  core.setOutput("new-version", newVersion);
+  
+} catch (error) {
+  core.setFailed(error.message);
+}
